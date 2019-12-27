f55204ef29b1cab26361adb0b67d32ad16f0b3af
Vladyslav Kolomiets <vladyslavkolomiets@gmail.com>
2019-12-27 11:45:29 +0200

    installed CocoaPods

---
+++ Declaration/Declaration/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Declaration
+//
+//  Created by Vladyslav Kolomiets on 12/27/19.
+//  Copyright © 2019 Vladyslav Kolomiets. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.windowScene = windowScene
+        //window?.rootViewController = MainTabBarController()
+        window?.makeKeyAndVisible()
+    }
+}
